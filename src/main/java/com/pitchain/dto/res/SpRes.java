5fcf627aae1eb05bffa6733af17865f1f214a0e5
LSH <lsh2613@naver.com>
2025-01-03 18:06:48 +0900

    mod: sp 조회 api 반환값 수정
    - Category 추가
    - Company 추가

--- src/main/java/com/pitchain/dto/res/SpRes.java
+++ src/main/java/com/pitchain/dto/res/SpRes.java
@@ -1,5 +1,7 @@
 package com.pitchain.dto.res;
 
+import com.pitchain.common.constant.BmCategory;
+import com.pitchain.entity.Bm;
 import com.pitchain.entity.Sp;
 import lombok.Builder;
 
@@ -9,15 +11,20 @@ public record SpRes(
         String shortPitchURL,
         String thumbnailImg,
         int views,
-        String name
+        String name,
+        BmCategory category,
+        String company
 ) {
     public static SpRes createRes(Sp sp) {
+        Bm bm = sp.getBm();
         return SpRes.builder()
                 .bmId(sp.getBm().getId())
                 .shortPitchURL(sp.getShortPitchURL())
                 .thumbnailImg(sp.getThumbnailImg())
                 .views(sp.getViews())
                 .name(sp.getName())
+                .category(bm.getCategory())
+                .company(bm.getCompany())
                 .build();
     }
 }
