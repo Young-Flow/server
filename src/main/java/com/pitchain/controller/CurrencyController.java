9fb53dea8301ca240b4189bf4bcf546e2048f619
Yunwoo Jeong <jeongmallro99@gmail.com>
2025-01-04 14:29:46 +0900

    refactor: 환전 api 로직 수정 - sixth commit
    - 테스트용 코드 삭제

--- src/main/java/com/pitchain/controller/CurrencyController.java
+++ src/main/java/com/pitchain/controller/CurrencyController.java
@@ -3,7 +3,6 @@ package com.pitchain.controller;
 import com.pitchain.common.apiPayload.dto.CustomApiResponse;
 import com.pitchain.dto.CurrencyDto;
 import com.pitchain.service.CurrencyService;
-import com.pitchain.service.ExchangeRateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class CurrencyController {
 
     private final CurrencyService currencyService;
-    private final ExchangeRateService exchangeRateService;
 
     @GetMapping("/currency")
     public CustomApiResponse calculateExchangeRate(@AuthenticationPrincipal Long memberId,
@@ -28,11 +26,5 @@ public class CurrencyController {
         result.setExchangeRateUpdateDateTime(exchangeRateUpdateDateTime);
 
         return CustomApiResponse.onSuccess(result);
-    }
-
-    @GetMapping("/cur")
-    public CustomApiResponse calculateExchangeRate(@AuthenticationPrincipal Long memberId) {
-        exchangeRateService.updateExchangeRateMap();
-        return CustomApiResponse.onSuccess();
     }
 }
