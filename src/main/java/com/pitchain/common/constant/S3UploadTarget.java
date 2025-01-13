3cabed89ca1944fbfc15f7d90294d696ee6c4c9c
LSH <lsh2613@naver.com>
2025-01-13 19:47:29 +0900

    mod: 누락된 파일 추가

--- src/main/java/com/pitchain/common/constant/S3UploadTarget.java
+++ src/main/java/com/pitchain/common/constant/S3UploadTarget.java
@@ -1,9 +1,30 @@
 package com.pitchain.common.constant;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum S3UploadTarget {
+
     // COMPANY
-    COMPANY_DESC, COMPANY_LOGO, COMPANY_PT, COMPANY_THUMBNAIL, COMPANY_MP4,
+    COMPANY_DESC(MIME.IMAGE), COMPANY_LOGO(MIME.IMAGE), COMPANY_PT(MIME.IMAGE), COMPANY_THUMBNAIL(MIME.IMAGE), COMPANY_VIDEO(MIME.VIDEO),
 
     // MEMBER
-    MEMBER_PROFILE,
+    MEMBER_PROFILE(MIME.IMAGE);
+
+    private final MIME mime;
+
+    public boolean isValidMimeType(String contentType) {
+        return contentType != null && contentType.startsWith(this.mime.getType());
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    public enum MIME {
+        IMAGE("image"),
+        VIDEO("video");
+
+        private final String type;
+    }
 }
