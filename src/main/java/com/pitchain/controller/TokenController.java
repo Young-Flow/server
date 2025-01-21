a10661eb72f77f64c960e96e92fee4059a3b9890
LSH <lsh2613@naver.com>
2025-01-21 19:35:32 +0900

    docs: TokenController Swagger 설명 추가

--- src/main/java/com/pitchain/controller/TokenController.java
+++ src/main/java/com/pitchain/controller/TokenController.java
@@ -3,11 +3,13 @@ package com.pitchain.controller;
 import com.pitchain.common.apiPayload.dto.CustomApiResponse;
 import com.pitchain.jwt.TokenUtil;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "JWT 발급")
 @RequiredArgsConstructor
 @RestController
 public class TokenController {
