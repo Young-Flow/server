06a71d81d010e5de5b08e44a10291c1cb8801f19
LSH <lsh2613@naver.com>
2025-01-21 19:35:57 +0900

    test: sentry 에러 테스트 api 추가

--- src/main/java/com/pitchain/common/apiPayload/statusEnums/ErrorStatus.java
+++ src/main/java/com/pitchain/common/apiPayload/statusEnums/ErrorStatus.java
@@ -9,36 +9,42 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorStatus implements ResponseStatus {
 
-    // common
+    // common,
     _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
     _BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다."),
     _UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다."),
     _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
 
+    // test
+    TEST_ERROR(HttpStatus.BAD_REQUEST, "TEST", "테스트 에러입니다."),
+
     // jwt
-    MISSING_ACCESS_TOKEN(HttpStatus.BAD_REQUEST, "TOKEN4012", "Access Token이 존재하지 않습니다."),
+    MISSING_ACCESS_TOKEN(HttpStatus.BAD_REQUEST, "TOKEN4012","Access Token이 존재하지 않습니다."),
 
     // member
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER4041", "존재하지 않는 사용자입니다."),
-    MEMBER_FORBIDDEN(HttpStatus.FORBIDDEN, "MEMBER4031", "사용자에게 권한이 없습니다."),
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER4041","존재하지 않는 사용자입니다."),
+
+    MEMBER_FORBIDDEN(HttpStatus.FORBIDDEN, "MEMBER4031","사용자에게 권한이 없습니다."),
 
     // bm
-    BM_NOT_FOUND(HttpStatus.NOT_FOUND, "BM4041", "존재하지 않는 BM입니다."),
+    BM_NOT_FOUND(HttpStatus.NOT_FOUND, "BM4041","존재하지 않는 BM입니다."),
 
     // sp
-    SP_NOT_FOUND(HttpStatus.NOT_FOUND, "SP4041", "존재하지 않는 SP입니다."),
+    SP_NOT_FOUND(HttpStatus.NOT_FOUND, "SP4041","존재하지 않는 SP입니다."),
 
     // comment
-    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMENT4041", "존재하지 않는 댓글입니다."),
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMENT4041","존재하지 않는 댓글입니다."),
 
     // S3
-    FAIL_S3_UPLOAD(HttpStatus.BAD_REQUEST, "S3_4001", "S3 업로드에 실패했습니다."),
-    INVALID_BUCKET_URL(HttpStatus.BAD_REQUEST, "S3_4002", "유효하지 않은 버킷 URL입니다."),
+    FAIL_S3_UPLOAD(HttpStatus.BAD_REQUEST, "S3_4001","S3 업로드에 실패했습니다."),
+
+    INVALID_BUCKET_URL(HttpStatus.BAD_REQUEST, "S3_4002","유효하지 않은 버킷 URL입니다."),
 
     // FILE
-    INVALID_MIME_TYPE(HttpStatus.BAD_REQUEST, "FILE4001", "유효하지 않은 파일 형식입니다."),
-    FAIL_STREAM_CONVERT(HttpStatus.BAD_REQUEST, "FILE4002", "스트림 변환에 실패했습니다."),
-    ;
+    INVALID_MIME_TYPE(HttpStatus.BAD_REQUEST, "FILE4001","유효하지 않은 파일 형식입니다."),
+
+    FAIL_STREAM_CONVERT(HttpStatus.BAD_REQUEST, "FILE4002","스트림 변환에 실패했습니다."),
+            ;
 
     private final HttpStatus httpStatus;
     private final String code;
