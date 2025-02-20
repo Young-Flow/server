5bf165a84022b33d9286ccecf4a642b210087522
Yunwoo Jeong <jeongmallro99@gmail.com>
2025-02-20 14:10:54 +0900

    feat: 새로운 CI/CD 구축 - seventh commit
    - start.sh, validate.sh 파일 내용 수정

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -6,6 +6,6 @@ JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
-
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+sleep 3
+CURRENT_PID=$(pgrep -f $JAR_FILE | tail -n 1)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
