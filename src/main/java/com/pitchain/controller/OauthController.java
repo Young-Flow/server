5f7f1d3caa8c167a89053e6659967170b2803124
Yunwoo Jeong <jeongmallro99@gmail.com>
2025-03-07 17:29:54 +0900

    Update src/main/java/com/pitchain/controller/OauthController.java

--- src/main/java/com/pitchain/controller/OauthController.java
+++ src/main/java/com/pitchain/controller/OauthController.java
@@ -22,7 +22,7 @@ public class OauthController {
     @Operation(summary = "소셜 로그인", description = "각 Oauth 플랫폼의 endpoint를 통해 code값  조회 후 api 호출 <br>" +
             "KAKAO: https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=891063e7c569324189a0353dfb18c534&redirect_uri={redirectURL} <br> " +
             "NAVER: https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=T4z00JRW0P38KpqrIc6w&redirect_uri={redirectURL} <br> " +
-            "GOOGLE: https://accounts.google.com/o/oauth2/v2/auth?client_id=936222247413-8cojftn1k2kipmrgspo9glprk5drrt3t.apps.googleusercontent.com&redirect_uri=http://localhost:8080&response_type=code&scope=email%20profile"
+            "GOOGLE: https://accounts.google.com/o/oauth2/v2/auth?client_id=936222247413-8cojftn1k2kipmrgspo9glprk5drrt3t.apps.googleusercontent.com&redirect_uri={redirectURL}&response_type=code&scope=email%20profile"
     )
     @ApiResponses(value = {
             @ApiResponse(responseCode = "COMMON200", description = "로그인 성공"),
