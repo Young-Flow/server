2a2b78c30b3406aef2ee29e245b9451b3e3fd751
LSH <lsh2613@naver.com>
2025-03-07 21:25:42 +0900

    docs: Res 설명 추가

--- src/main/java/com/pitchain/dto/res/LoginRes.java
+++ src/main/java/com/pitchain/dto/res/LoginRes.java
@@ -1,12 +1,15 @@
 package com.pitchain.dto.res;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class LoginRes {
+    @Schema(required = true)
     private String accessToken;
+    @Schema(required = true)
     private String refreshToken;
 
     public static LoginRes createRes(String accessToken, String refreshToken) {
