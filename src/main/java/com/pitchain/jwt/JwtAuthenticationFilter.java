41ab8ec2d2d88ab2d03cc967d61f11ddf1ac6b86
LSH <lsh2613@naver.com>
2025-03-09 14:53:33 +0900

    refactor: jwt claim 저장 객체 수정
    - (Long id, String memberRole) -> (Long id, MemberRole role)

--- src/main/java/com/pitchain/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/pitchain/jwt/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.pitchain.jwt;
 
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.pitchain.common.apiPayload.statusEnums.ErrorStatus;
+import com.pitchain.common.constant.MemberRole;
 import com.pitchain.common.constant.TokenType;
 import com.pitchain.common.exception.GeneralHandler;
 import jakarta.servlet.FilterChain;
@@ -50,7 +51,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         Long id = decodedJWT.getClaim("id").asLong();
         String role = decodedJWT.getClaim("role").asString();
 
-        MemberDetails memberDetails = new MemberDetails(id, role);
+        MemberDetails memberDetails = new MemberDetails(id, MemberRole.toEnum(role));
         Authentication authentication = new UsernamePasswordAuthenticationToken(memberDetails, null);
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
