23861d5d70b18372630fa69265dbab668eb2ff1b
LSH <lsh2613@naver.com>
2025-03-12 17:41:45 +0900

    refactor: ai 관련 로직 제거

--- src/main/java/com/pitchain/repository/MemberRepository.java
+++ src/main/java/com/pitchain/repository/MemberRepository.java
@@ -1,30 +1,12 @@
 package com.pitchain.repository;
 
-import com.pitchain.dto.PreferenceInfoDto;
 import com.pitchain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String email);
 
-    @Query("""
-                SELECT new com.pitchain.dto.PreferenceInfoDto(
-                    m.id,
-                    bm.id,
-                    COALESCE(sp.viewTime, 0),
-                    CASE WHEN mbh.id IS NOT NULL THEN true ELSE false END,
-                    CASE WHEN i.id IS NOT NULL THEN true ELSE false END
-                )
-                FROM Member m
-                CROSS JOIN Bm bm
-                LEFT JOIN MyBmHistory mbh ON bm.id = mbh.bm.id AND mbh.member.id = m.id
-                LEFT JOIN MySpHistory sp ON bm.id = sp.bm.id AND sp.member.id = m.id
-                LEFT JOIN Investment  i ON bm.id = i.bm.id AND i.member.id = m.id
-            """)
-    List<PreferenceInfoDto> getMemberPreferenceInfos();
 }
