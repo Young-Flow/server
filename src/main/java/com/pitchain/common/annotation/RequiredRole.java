4e7aeb0586972715ea6e7d0a4df6f51d9a73b195
seungheonlee <lsh2613@naver.com>
2025-06-28 01:25:32 +0900

    feat: 리플렉션을 통한 API 권한 동적 설정
    - MemberRole.MEMBER 추가
    - MemberRole 타입별 권한부여
    - @RequiredRole 구현
    - JWT 인증 시 Authentication에 권한 추가
    - 리플렉션을 통한 권한 API 수집

---
+++ src/main/java/com/pitchain/common/annotation/RequiredRole.java
@@ -0,0 +1,14 @@
+package com.pitchain.common.annotation;
+
+import com.pitchain.common.constant.MemberRole;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequiredRole {
+    MemberRole value();
+}
