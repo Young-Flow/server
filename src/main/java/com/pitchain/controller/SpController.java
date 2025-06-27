e5deec0c0e5426e10f0711b2adae13211eab0d49
seungheonlee <lsh2613@naver.com>
2025-06-28 03:28:24 +0900

    refactor: RESTFul API로 수정

--- src/main/java/com/pitchain/controller/SpController.java
+++ src/main/java/com/pitchain/controller/SpController.java
@@ -19,15 +19,15 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RequiredArgsConstructor
-@RequestMapping("/sps")
 @RestController
 public class SpController {
     private final SpService spService;
 
     @RequiredRole(MemberRole.COMPANY)
     @Operation(summary = "SP 생성")
-    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    @PostMapping(value = "/bms/{bmId}/sps", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public void createSp(@AuthenticationPrincipal MemberDetails memberDetails,
+                         @PathVariable Long bmId,
                          @Valid @RequestPart SpCreateReq spCreateReq,
                          @RequestPart MultipartFile thumbnailImg) {
         spService.createSp(memberDetails, spCreateReq, thumbnailImg);
@@ -41,7 +41,7 @@ public class SpController {
     }
 
     @Operation(summary = "카테고리별 SP 리스트 조회", description = "첫 조회 시 쿼리 파라미터에 lastSpId를 포함시키지 않습니다.")
-    @GetMapping("/category")
+    @GetMapping("/sps")
     public InfinityScrollRes<SpDetailRes> getSpDetailsFilteredCategory(@AuthenticationPrincipal MemberDetails memberDetails,
                                                                        @RequestParam(required = false) String mainCategoryInKorean,
                                                                        @RequestParam(required = false) Long lastSpId,
@@ -51,17 +51,20 @@ public class SpController {
     }
 
     @Operation(summary = "SP 상세 조회")
-    @GetMapping("/{spId}")
-    public SpDetailRes getSpDetail(@AuthenticationPrincipal MemberDetails memberDetails, @PathVariable Long spId) {
+    @GetMapping("/bms/{bmId}/sps/{spId}")
+    public SpDetailRes getSpDetail(@AuthenticationPrincipal MemberDetails memberDetails,
+                                   @PathVariable Long bmId,
+                                   @PathVariable Long spId) {
         SpDetailRes spDetailRes = spService.getSpDetail(memberDetails, spId);
         return spDetailRes;
     }
 
     @RequiredRole(MemberRole.COMPANY)
     @Operation(summary = "SP 수정")
-    @PutMapping(value = "/{spId}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    @PutMapping(value = "/bms/{bmId}/sps/{spId}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public void updateSp(@AuthenticationPrincipal MemberDetails memberDetails,
                          @PathVariable Long spId,
+                         @PathVariable Long bmId,
                          @NotBlank @RequestPart String name,
                          @RequestPart(required = false) MultipartFile thumbnailImg) {
         spService.updateSp(memberDetails, spId, name, thumbnailImg);
@@ -69,8 +72,10 @@ public class SpController {
 
     @RequiredRole(MemberRole.COMPANY)
     @Operation(summary = "SP 삭제")
-    @DeleteMapping("/{spId}")
-    public void deleteSp(@AuthenticationPrincipal MemberDetails memberDetails, @PathVariable Long spId) {
+    @DeleteMapping("/bms/{bmId}/sps/{spId}")
+    public void deleteSp(@AuthenticationPrincipal MemberDetails memberDetails,
+                         @PathVariable Long bmId,
+                         @PathVariable Long spId) {
         spService.deleteSp(memberDetails, spId);
     }
 }
