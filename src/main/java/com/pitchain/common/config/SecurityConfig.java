6074af09eed758b27e45aa402d33ae9c0f8f0322
Yunwoo Jeong <jeongmallro99@gmail.com>
2025-07-17 01:14:50 +0900

    chore: Origins 설정 주입으로 변경

--- src/main/java/com/pitchain/common/config/SecurityConfig.java
+++ src/main/java/com/pitchain/common/config/SecurityConfig.java
@@ -91,7 +91,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:8080", "https://localhost:8080"));
+        config.setAllowedOrigins(allowedOrigins);
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(Arrays.asList("Authorization", "Authorization-Refresh"));
